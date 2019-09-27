aa0c64b33f8dfd16511e2304c632411dcd2eac81
Yujung <dongyujung@gmail.com>
2019-09-27 10:43:16 -0700

    update

--- connectors/topic_to_postgres.py
+++ connectors/topic_to_postgres.py
@@ -35,6 +35,7 @@ consumer = KafkaConsumer(
     enable_auto_commit=True,
     group_id='my-group',
     value_deserializer=lambda x: json.loads(x.decode('utf-8'))
+)
 
 for message in consumer:
     print(message)
