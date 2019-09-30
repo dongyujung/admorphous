aca6b29e8ee040ac5caf44fb26750788b51cd53f
Yujung <dongyujung@gmail.com>
2019-09-29 18:31:04 -0700

    updated one node sh

--- test_one_node_server.sh
+++ test_one_node_server.sh
@@ -1,4 +1,4 @@
-export KAFKA_HOME=~/kafka_2.12-2.3.0
+export KAFKA_HOME=~/kafka_2.12-2.3.0 &
 $KAFKA_HOME/bin/zookeeper-server-start.sh $KAFKA_HOME/config/zookeeper.properties &
 $KAFKA_HOME/bin/kafka-server-start.sh $KAFKA_HOME/config/server.properties &
 $KAFKA_HOME/bin/kafka-topics.sh --create \
