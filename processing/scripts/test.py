7c440d647a687edb3489f3e04468a1da6ed779b3
Yujung <dongyujung@gmail.com>
2019-09-29 18:59:22 -0700

    updated one node sh

--- processing/scripts/test.py
+++ processing/scripts/test.py
@@ -4,6 +4,7 @@ app = faust.App(
     'first-app',
     broker='kafka://localhost:9092',
     topic_partitions=1,
+    value_serializer='json'
 )
 
 class PageView(faust.Record):
@@ -14,15 +15,22 @@ class PageView(faust.Record):
     geo_location: str
     traffic_source: int
 
+class Platform(faust.Record):
+    platform: int
+    count: int
+
 page_view_topic = app.topic('test', value_type=PageView)
 
-platform_table = app.Table('page_views', default=int)
+platform_table = app.Table('platform_table', default=int)
+platform_topic = app.topic('platform', value_type=Platform)
 
 @app.agent(page_view_topic)
 async def count_page_views(views):
     async for view in views.group_by(PageView.platform):
-        platform_table["platform"] = view.platform
-        platform_table["count"] += 1
-        #platform_table[view.platform] += 1
+        #platform_table["platform"] = view.platform
+        #platform_table["count"] += 1
+        platform_table[view.platform] += 1
+        platform_topic["platform"] = view.platform
+        platform_topic["count"] = platform_table[view.platform]
 
 
