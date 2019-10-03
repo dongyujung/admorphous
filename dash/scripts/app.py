5aa61fff5e728a34b03dd127406203777a85866e
Yujung <dongyujung@gmail.com>
2019-10-03 00:20:55 -0700

    dash test postgres query

--- dash/scripts/app.py
+++ dash/scripts/app.py
@@ -14,65 +14,46 @@ db_type = "postgres"
 usr = config.username
 pwrd = config.password
 
-connection = psycopg2.connect(user=usr,
-                              password=pwrd,
-                              host=db_host_ip,
-                              port=db_port,
-                              database=db_type)
+connection = None
+cursor = None
 
-cursor = connection.cursor()
+try:
+    connection = psycopg2.connect(user=usr,
+                                  password=pwrd,
+                                  host=db_host_ip,
+                                  port=db_port,
+                                  database=db_type)
+    cursor = connection.cursor()
+    query1 = "SELECT id, count FROM test WHERE platform = 2;"
+    cursor.execute(query1)
+    rows = cursor.fetchall()
 
-query1 = "SELECT id, count FROM test " \
-         "WHERE platform = 2;"
+    [pvid, count] = map(list, zip(*rows))
+    print(pvid)
+    print(count)
+except Exception as e:
+    print(e)
+finally:
+    cursor.close()
+    connection.close()
 
 external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
 
 app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
 
-app.layout = html.Div(children=[
-    html.H1(children='Hello Dash'),
+trace_1 = go.Scatter(
+    x = pvid, y = count,
+    name = 'counts',
+    line = {width = 2, color = 'rgb(229, 151, 50)'}
+)
+layout = go.Layout(title = 'Pageview Counts',
+                   hovermode = 'closest')
+fig = go.Figure(data = [trace_1], layout = layout)
 
-    html.Div(children='''
-        Dash: A web application framework for Python.
-    '''),
-
-    dcc.Graph(
-        id='example-graph',
-        figure={
-            'data': [
-                {'x': [1, 2, 3], 'y': [4, 1, 2], 'type': 'bar', 'name': 'SF'},
-                {'x': [1, 2, 3], 'y': [2, 4, 5], 'type': 'bar', 'name': u'Montréal'},
-            ],
-            'layout': {
-                'title': 'Dash Data Visualization'
-            }
-        }
-    )
-])
-
-go.Scatter(x)
-
-
-def create_time_series(dff, axis_type, title):
-    return {
-        'data': [go.Scatter(
-            x=dff['Year'],
-            y=dff['Value'],
-            mode='lines'
-        )],
-        'layout': {
-            'height': 225,
-            'margin': {'l': 20, 'b': 30, 'r': 10, 't': 10},
-            'annotations': [{
-                'x': 0, 'y': 0.85, 'xanchor': 'left', 'yanchor': 'bottom',
-                'xref': 'paper', 'yref': 'paper', 'showarrow': False,
-                'align': 'left', 'bgcolor': 'rgba(255, 255, 255, 0.5)',
-                'text': title
-            }],
-            'yaxis': {'type': 'linear' if axis_type == 'Linear' else 'log'},
-            'xaxis': {'showgrid': False}
-        }
-    }
+# Create a layout
+app.layout = html.Div([
+                dcc.Graph(id = 'plot', figure = fig)
+                      ])
 
 if __name__ == '__main__':
     app.run_server(debug=True, port=8080, host='0.0.0.0')
