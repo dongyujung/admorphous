ca8f34aa55a3cd9fddaaaa5d5a9abeedef15ea3c
Yujung <dongyujung@gmail.com>
2019-10-02 22:25:05 -0700

    dash test postgres query

--- dash/scripts/query_postgres.py
+++ dash/scripts/query_postgres.py
@@ -10,23 +10,25 @@ usr = config.username
 pwrd = config.password
 
 try:
+    print("1")
     connection = psycopg2.connect(user=usr,
                                   password=pwrd,
                                   host=db_host_ip,
                                   port=db_port,
                                   database=db_type)
-
+    print("2")
     cursor = connection.cursor()
-
+    print(3)
     query1 = "SELECT id, count FROM test WHERE platform = 2;"
-
+    print(4)
     result = cursor.execute(query1)
+    print(5)
     rows = result.fetchall()
+    print(6)
     print(rows)
 
     connection.commit()
-    cursor.close()
-    connection.close()
+    print(7)
 except Exception as e: print(e)
 finally:
     cursor.close()
