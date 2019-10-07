c1f2cd49a2e88a28f8002b5fed2f40ee638ade04
Yujung <dongyujung@gmail.com>
2019-10-06 23:13:24 -0700

    updates on ksql queries

--- ksql/scripts/create_streams.sh
+++ ksql/scripts/create_streams.sh
@@ -1,5 +1,5 @@
 ksql http://localhost:8088 <<EOF
-> RUN SCRIPT '~/admorphous/ksql/views_page.sql';
-> RUN SCRIPT '~/admorphous/ksql/impressions_ad.sql';
-> exit
-> EOFs
+RUN SCRIPT '~/admorphous/ksql/views_page.sql';
+RUN SCRIPT '~/admorphous/ksql/impressions_ad.sql';
+exit
+EOFs
