33707c125ab8d965c9b6c9c225b60f0c25095677
Yujung <dongyujung@gmail.com>
2019-10-13 20:56:30 -0700

    process streams file location update

--- process_streams.sh
+++ process_streams.sh
@@ -1,5 +1,5 @@
 ksql http://localhost:8088 <<EOF
-RUN SCRIPT '~/admorphous/ksql/scripts/views_page.sql';
-RUN SCRIPT '~/admorphous/ksql/scripts/impressions_ad.sql';
+RUN SCRIPT './ksql/scripts/views_page.sql';
+RUN SCRIPT './ksql/scripts/impressions_ad.sql';
 exit
 EOFs
--- start_zookeeper.sh
+++ start_zookeeper.sh
@@ -1,3 +1,2 @@
 KAFKA_HOME=~/kafka_2.12-2.3.0
-screen -S zookeeper
-$KAFKA_HOME/bin/zookeeper-server-start.sh $KAFKA_HOME/config/zookeeper.properties
+nohup $KAFKA_HOME/bin/zookeeper-server-start.sh $KAFKA_HOME/config/zookeeper.properties
