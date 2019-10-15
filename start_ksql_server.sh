01d44279a199511236de110dc92b75d9cf9a9ec9
Yujung <dongyujung@gmail.com>
2019-10-14 23:06:22 -0700

    updates in ksql threads

--- start_ksql_server.sh
+++ start_ksql_server.sh
@@ -1,3 +1,4 @@
 export JMX_PORT=9999 && \
 export JAVA_HOME=/usr/lib/jvm/java-8-openjdk-amd64 && \
+KSQL_OPTS="-Dksql.streams.num.streams.threads=4" && \
 /usr/bin/ksql-server-start /etc/ksql/ksql-server.properties
