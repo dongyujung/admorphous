81732144fae7549c61506e268fc2e0eb0d58b059
Yujung <dongyujung@gmail.com>
2019-10-28 11:31:27 -0700

    query update

--- connectors/scripts/impressions_to_postgres.py
+++ connectors/scripts/impressions_to_postgres.py
@@ -44,11 +44,13 @@ try:
 
     # Receiving and accumulating 20 messages at a time and inserting into db
     i = 0
+    # append each row as set in rows list
     rows = []
     for message in consumer:
         i += 1
         inbound_dict = message.value
 
+        # Row to be inserted
         row = (inbound_dict['AD_ID'],
                                inbound_dict['COUNT'],
                                datetime.fromtimestamp(message.timestamp / 1e3),
@@ -56,6 +58,7 @@ try:
         print(row)
         rows.append(row)
 
+        # Commit insert for every 20 messages received
         if i % 20 == 0:
             rows_template = ','.join(['%s'] * len(rows))
             insert_query = 'rows_temp = ','.join(['%s'] * len(rows))
@@ -63,13 +66,6 @@ try:
                            'VALUES {};'.format(rows_template)
             cursor.execute(insert_query, rows)
             connection.commit()
-            i = 0
-            del rows
-            rows = [] ' \
-                           'VALUES {};'.format(rows_temp)
-            cursor.execute(insert_query, rows)
-            connection.commit()
-            i = 0
             del rows
             rows = []
 
--- connectors/scripts/pageview_to_postgres.py
+++ connectors/scripts/pageview_to_postgres.py
@@ -44,12 +44,14 @@ try:
 
     # Receiving and accumulating 20 messages at a time and inserting into db
     i = 0
+    # append each row as set in rows list
     rows = []
     for message in consumer:
 
         i += 1
         inbound_dict = message.value
 
+        # Row to be inserted
         row = (inbound_dict['DOCUMENT_ID'],
                inbound_dict['COUNT'],
                datetime.fromtimestamp(message.timestamp / 1e3),
@@ -58,13 +60,13 @@ try:
         print(row)
         rows.append(row)
 
+        # Commit insert for every 20 messages received
         if i % 20 == 0:
             rows_template = ','.join(['%s'] * len(rows))
             insert_query = 'INSERT INTO views_page (document_id, count, produce_time, consume_time, window_end) ' \
                            'VALUES {};'.format(rows_template)
             cursor.execute(insert_query, rows)
             connection.commit()
-            i = 0
             del rows
             rows = []
 
